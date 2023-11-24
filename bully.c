4a88e30416b65926550997743b73636514c3f402
Ginseng Strip <72723461+gaurabkhakhlari@users.noreply.github.com>
2023-11-24 11:14:05 +0530

    Update bully.c

--- bully.c
+++ bully.c
@@ -3,8 +3,6 @@
 #include <pthread.h>
 #include <unistd.h>
 
-#define NUM_PROCESSES 5
-
 typedef struct
 {
     int id;
@@ -12,7 +10,8 @@ typedef struct
     pthread_t thread_id;
 } Process;
 
-Process processes[NUM_PROCESSES];
+Process *processes;
+int num_processes;
 
 void *process_function(void *arg)
 {
@@ -40,7 +39,7 @@ void initiate_election(int failed_process_id)
     int highest_id = -1;
     int new_leader_id = -1;
 
-    for (int i = 0; i < NUM_PROCESSES; ++i)
+    for (int i = 0; i < num_processes; ++i)
     {
         if (processes[i].id > highest_id && processes[i].id > failed_process_id)
         {
@@ -58,8 +57,13 @@ void initiate_election(int failed_process_id)
 
 int main()
 {
+    printf("Enter the number of processes: ");
+    scanf("%d", &num_processes);
+
+    processes = (Process *)malloc(num_processes * sizeof(Process));
+
     // Initialize processes
-    for (int i = 0; i < NUM_PROCESSES; ++i)
+    for (int i = 0; i < num_processes; ++i)
     {
         processes[i].id = i;
         processes[i].is_leader = 0;
@@ -67,13 +71,24 @@ int main()
     }
 
     // Simulate a process failure (e.g., process 3 fails)
-    int failed_process_id = 3;
+    int failed_process_id;
+    printf("Enter the ID of the failed process: ");
+    scanf("%d", &failed_process_id);
 
-    // Initiate election when a process fails
-    initiate_election(failed_process_id);
+    if (failed_process_id >= 0 && failed_process_id < num_processes)
+    {
+        // Initiate election when a process fails
+        initiate_election(failed_process_id);
 
-    // Let the remaining processes continue their work
-    sleep(10);
+        // Let the remaining processes continue their work
+        sleep(10);
+    }
+    else
+    {
+        printf("Invalid process ID.\n");
+    }
+
+    free(processes);
 
     return 0;
 }
